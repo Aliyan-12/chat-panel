873feeb041a24ebce509c23e88937d1bd4c8ace9
Aliyan-12 <alyannasir225@gmail.com>
2025-06-16 21:48:49 +0500

    added channels for groups.

---
+++ app/Events/NewMessage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Conversation;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewMessage
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $conversation = null;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Conversation $conversation)
+    {
+        $this->conversation = $conversation;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('groups.'. $this->conversation->group->id),
+        ];
+    }
+}
